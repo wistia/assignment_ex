83804db3ccfff4b86cd6965ab8e49a0520b720f4
Josh Bodah <jb3689@yahoo.com>
2017-10-04 13:43:04 -0400

    pull up types into Assignment

--- lib/assignment.ex
+++ lib/assignment.ex
@@ -1,10 +1,15 @@
 defmodule Assignment do
-  @spec round_robin(list(Assignment.Result.assignee), list(Assignment.Result.destination), opts :: map) :: Assignment.Result.t
+  @type destination :: any()
+  @type assignee :: any()
+  @type assignments :: %{destination => list(assignee)}
+  @type assignment :: {destination, assignee}
+
+  @spec round_robin(list(Assignment.assignee), list(Assignment.destination), opts :: map) :: Assignment.Result.t
   def round_robin(unassigned, destinations, opts \\ []) do
     Assignment.RoundRobin.assign(unassigned, destinations, opts)
   end
 
-  @spec cached_round_robin(list(Assignment.Result.assignee), list(Assignment.Result.destination), opts :: map) :: Assignment.Result.t
+  @spec cached_round_robin(list(Assignment.assignee), list(Assignment.destination), opts :: map) :: Assignment.Result.t
   def cached_round_robin(unassigned, destinations, opts \\ []) do
     Assignment.CachedRoundRobin.assign(unassigned, destinations, opts)
   end
--- lib/assignment/result.ex
+++ lib/assignment/result.ex
@@ -1,12 +1,8 @@
 defmodule Assignment.Result do
-  @type destination :: any()
-  @type assignee :: any()
-  @type assignments :: %{destination => list(assignee)}
-  @type assignment :: {destination, assignee}
-  @type t :: %Assignment.Result{unassigned: list(assignee), assignments: assignments, destinations: list(destination)}
+  @type t :: %Assignment.Result{unassigned: list(Assignment.assignee), assignments: Assignment.assignments, destinations: list(Assignment.destination)}
   defstruct [:unassigned, :assignments, :destinations]
 
-  @spec add_assignment(assignments, assignment) :: assignments
+  @spec add_assignment(Assignment.assignments, Assignment.assignment) :: Assignment.assignments
   def add_assignment(assignments, {destination, assignee}) do
     case assignments[destination] do
       nil -> put_in(assignments[destination], [assignee])
