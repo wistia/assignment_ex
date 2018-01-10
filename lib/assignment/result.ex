69aacb9fea879527829158642a28e359acb369a1
Josh Bodah <joshuabodah@gmail.com>
2018-01-10 11:29:50 -0500

    add state to Assignment.Result

--- lib/assignment/result.ex
+++ lib/assignment/result.ex
@@ -1,6 +1,27 @@
 defmodule Assignment.Result do
-  @type t :: %Assignment.Result{unassigned: list(Assignment.assignee), assignments: Assignment.assignments, destinations: list(Assignment.destination)}
-  defstruct [:unassigned, :assignments, :destinations]
+  @moduledoc """
+  Data structure returned after assignments occur
+
+  ## Fields
+
+  * `assignments` - a map of destinations to input assigned to those destinations
+
+  * `destinations` - all of the destinations provided
+
+  * `unassigned` - input we wanted to assign but that didn't get assigned (e.g. configured not to loop)
+
+  * `state` - allows for assignment to be continued from where it left off. This allows us to do things like ensure
+      we round-robin fairly between desintations over multiple calls
+  """
+
+  @type t :: %Assignment.Result{
+    unassigned: list(Assignment.assignee),
+    assignments: Assignment.assignments,
+    destinations: list(Assignment.destination),
+    state: map :: any()
+  }
+
+  defstruct [:unassigned, :assignments, :destinations, :state]
 
   @spec add_assignment(Assignment.assignments, Assignment.assignment) :: Assignment.assignments
   def add_assignment(assignments, {destination, assignee}) do
